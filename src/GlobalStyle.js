617f9fe08e9ad009ab3c9b0d30b505c20bdff15d
Mandy Schwarze <mandy2301@me.com>
2021-04-26 20:58:03 +0200

    Add GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    h1 {
+        font-size: 2.5em;
+        color: #fb1;
+    }
+`
+
+export default GlobalStyle
